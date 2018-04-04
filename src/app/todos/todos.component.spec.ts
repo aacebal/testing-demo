70ca3c855638f9c4b8311d37fdfe443b2cd4af00
Adel Acebal <adelacebal@yahoo.com>
2018-04-04 19:44:22 -0400

    singleton

--- src/app/todos/todos.component.spec.ts
+++ src/app/todos/todos.component.spec.ts
@@ -1,6 +1,18 @@
+/* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { TodosComponent } from './todos.component';
+import { TodoService } from './todo.service';
+
+//NOTE: I've deliberately excluded this suite from running
+// because the test will fail. This is because we have not 
+// provided the TodoService as a dependency to TodosComponent. 
+// 
+// When you get to Lecture 6 (Providing Dependencies), be sure
+// to remove "x" from "xdescribe" below. 
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
@@ -8,7 +20,9 @@ describe('TodosComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TodosComponent ]
+      imports: [ HttpModule ], 
+      declarations: [ TodosComponent ],
+      providers: [ TodoService ]
     })
     .compileComponents();
   }));
@@ -21,5 +35,9 @@ describe('TodosComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load todos from the server', () => {
+    let service = TestBed.get(TodoService)
   });
 });
